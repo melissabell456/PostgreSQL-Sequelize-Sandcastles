709d09fe6628657aca96ed8cfd0810ae3d4e4f91
Melissa Bell <melissabell456@gmail.com>
2018-04-17 12:54:43 -0500

    added /beaches route to display all beaches

--- app.js
+++ app.js
@@ -10,10 +10,20 @@ const { Beach, Lifeguard } = require('./models');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// lifeguard routes
 app.get('/lifeguards', (req, res, next) => {
   Lifeguard.findAll()
   .then( lifeguards => {
     res.status(200).json(lifeguards);
+  })
+  .catch( err => { next(err) });
+})
+
+// beach routes
+app.get('/beaches', (req, res, next) => {
+  Beach.findAll()
+  .then( beaches => {
+    res.status(200).json(beaches);
   })
   .catch( err => { next(err) });
 })
